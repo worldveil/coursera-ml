497ae8b5f810e56c0afb9ed349d6cc713db986ac
Will Drevo <will.drevo@gmail.com>
2014-05-15 03:12:21 -0400

    cleaned up

--- logistic/code/costFunction.m
+++ logistic/code/costFunction.m
@@ -5,24 +5,15 @@ function [J, grad] = costFunction(theta, X, y)
     %   w.r.t. to the parameters.
 
     % Initialize some useful values
-    m = length(y); % number of training examples
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Compute the cost of a particular choice of theta.
-    %               You should set J to the cost.
-    %               Compute the partial derivatives and set grad to the partial
-    %               derivatives of the cost w.r.t. each parameter in theta
-    %
-    % Note: grad should have the same dimensions as theta
-    %
+    n = length(y); % number of training examples
     
     % cost: J
-    J = -1/m * sum(...
+    J = -1/n * sum(...
         y      .* log(sigmoid(X * theta)) + ...
         (1 - y) .* log(1 - sigmoid(X * theta)) ...
     );
 
     % gradient: compute as the derivative of the cost function
-    grad = 1/m * X' * ((sigmoid(X * theta)) - y);
+    grad = 1/n * X' * ((sigmoid(X * theta)) - y);
 
 end
--- logistic/code/costFunctionReg.m
+++ logistic/code/costFunctionReg.m
@@ -3,31 +3,20 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
     %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
     %   theta as the parameter for regularized logistic regression and the
     %   gradient of the cost w.r.t. to the parameters. 
-
-    % Initialize some useful values
-    m = length(y); % number of training examples
-
-    % You need to return the following variables correctly 
-    J = 0;
-    grad = zeros(size(theta));
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Compute the cost of a particular choice of theta.
-    %               You should set J to the cost.
-    %               Compute the partial derivatives and set grad to the partial
-    %               derivatives of the cost w.r.t. each parameter in theta
-   
+    
+    n = length(y); % number of training examples
+    
     % cost: J, this time with the penalty for the magnitude of theta
-    J = -1/m * sum(...
+    J = -1/n * sum(...
         y      .* log(sigmoid(X * theta)) + ...
         (1 - y) .* log(1 - sigmoid(X * theta)) ...
-    ) + lambda / (2 * m) * sum(theta(2:end) .* theta(2:end));
+    ) + lambda / (2 * n) * sum(theta(2:end) .* theta(2:end));
 
     % gradient: compute as the derivative of the cost function
-    grad = 1/m * X' * ((sigmoid(X * theta)) - y) + theta * lambda / m;
+    grad = 1/n * X' * ((sigmoid(X * theta)) - y) + theta * lambda / n;
    
     % we do not regularize the constant offset term, 
     % undo the gradient penalization for the constant x_0 term
-    grad(1) = grad(1) - lambda / m * theta(1);
+    grad(1) = grad(1) - lambda / n * theta(1);
     
 end
