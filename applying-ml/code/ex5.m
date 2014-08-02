2ce0b2554691e45eed924c8338c4e9e1e76c2248
Will Drevo <will.drevo@gmail.com>
2014-08-02 14:09:08 -0400

    Finished bias/variance excercise

--- applying-ml/code/ex5.m
+++ applying-ml/code/ex5.m
@@ -39,8 +39,8 @@ plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
 xlabel('Change in water level (x)');
 ylabel('Water flowing out of the dam (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =========== Part 2: Regularized Linear Regression Cost =============
 %  You should now implement the cost function for regularized linear 
@@ -53,8 +53,8 @@ J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 fprintf(['Cost at theta = [1 ; 1]: %f '...
          '\n(this value should be about 303.993192)\n'], J);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =========== Part 3: Regularized Linear Regression Gradient =============
 %  You should now implement the gradient for regularized linear 
@@ -68,8 +68,8 @@ fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
          '\n(this value should be about [-15.303016; 598.250744])\n'], ...
          grad(1), grad(2));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =========== Part 4: Train Linear Regression =============
@@ -93,8 +93,8 @@ hold on;
 plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
 hold off;
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =========== Part 5: Learning Curve for Linear Regression =============
@@ -122,8 +122,8 @@ for i = 1:m
     fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
 end
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =========== Part 6: Feature Mapping for Polynomial Regression =============
 %  One solution to this is to use polynomial regression. You should now
@@ -152,8 +152,8 @@ X_poly_val = [ones(size(X_poly_val, 1), 1), X_poly_val];           % Add Ones
 fprintf('Normalized Training Example 1:\n');
 fprintf('  %f  \n', X_poly(1, :));
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+%fprintf('\nProgram paused. Press enter to continue.\n');
+%pause;
 
 
 
@@ -192,8 +192,7 @@ for i = 1:m
     fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
 end
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+fprintf('Program paused. Press enter to continue.\n'); pause;
 
 %% =========== Part 8: Validation for Selecting Lambda =============
 %  You will now implement validationCurve to test various values of 
@@ -216,5 +215,5 @@ for i = 1:length(lambda_vec)
             lambda_vec(i), error_train(i), error_val(i));
 end
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
--- applying-ml/code/linearRegCostFunction.m
+++ applying-ml/code/linearRegCostFunction.m
@@ -1,37 +1,35 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
-%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
-%regression with multiple variables
-%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
-%   cost of using theta as the parameter for linear regression to fit the 
-%   data points in X and y. Returns the cost in J and the gradient in grad
+    %LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+    %regression with multiple variables
+    %   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+    %   cost of using theta as the parameter for linear regression to fit the 
+    %   data points in X and y. Returns the cost in J and the gradient in grad
 
-% Initialize some useful values
-m = length(y); % number of training examples
+    % Initialize some useful values
+    n = length(y); % number of training examples
+    grad = zeros(size(theta));
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
-
-
-
-
-
-
-
-
-
-
-
-
-% =========================================================================
-
-grad = grad(:);
-
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Compute the cost and gradient of regularized linear 
+    %               regression for a particular choice of theta.
+    %
+    %               You should set J to the cost and grad to the gradient.
+    %
+    
+    % calculate the cost using the squared error and the complexity
+    % regularization penalty
+    scaled_squared_error = sum(((X * theta - y) .^ 2)) / 2 / n;
+    regularization = lambda * sum(theta(2:end) .^ 2) / 2 / n;
+    J = scaled_squared_error + regularization;
+    
+    % now caluclate the gradient for each entry of theta 
+    error = X * theta - y;
+    for j = 1 : length(theta)
+        grad(j) = sum(error .* X(:, j)) / n;
+        if j > 1
+            grad(j) = grad(j) + lambda * theta(j) / n;
+        end
+    end
+    
+    grad = grad(:);  % unroll 
 end
